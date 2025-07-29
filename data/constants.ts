f6db8963d86595047b58592b533fd6c1235b9192
Tom Zhang <tommyawesome08@gmail.com>
2025-07-29 11:29:49 -0400

    correction for backend

--- data/constants.ts
+++ data/constants.ts
@@ -1,2 +1,2 @@
-// export const backend = 'https://thecolorprojectworld-orbitview.pythonanywhere.com'
-export const backend = "http://127.0.0.1:8000";
+export const backend = 'https://thecolorprojectworld-orbitview.pythonanywhere.com'
+// export const backend = "http://127.0.0.1:8000";
